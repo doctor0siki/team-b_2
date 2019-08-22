531fc5ec85fc009929af92d14b06b63599d0c16d
Kazuki-Takasaki <54342213+Kazuki-Takasaki@users.noreply.github.com>
2019-08-22 19:41:10 +0900

    問い合わせページ1/2作成

---
+++ app/Controller/company-import/company-import.php
@@ -0,0 +1,64 @@
+
+<?php
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Model\Dao\Company;
+
+
+// 会員登録ページコントローラ
+$app->get('/company-import/', function (Request $request, Response $response) {
+
+    //GETされた内容を取得します。
+    $data = $request->getQueryParams();
+
+    // Render index view
+    return $this->view->render($response, 'company-import/company-import.twig', $data);
+
+});
+
+// 会員登録処理コントローラ
+$app->post('/company-import/', function (Request $request, Response $response) {
+
+    //POSTされた内容を取得します
+    $data = $request->getParsedBody();
+    //dd($data);
+    //ユーザーDAOをインスタンス化
+    $company = new Company($this->db);
+
+/*
+    //入力されたメールアドレスの会員が登録済みかどうかをチェックします
+    if ($company->select(array("email" => $data["email"]), "", "", 1, false)) {
+
+        //入力項目がマッチしない場合エラーを出す
+        $data["error"] = "このメールアドレスは既に会員登録済みです";
+
+        // 入力フォームを再度表示します
+        return $this->view->render($response, 'register/register.twig', $data);
+
+    }
+*/
+    //DB登録に必要ない情報は削除します
+    //unset($data["password_re"]);
+
+    //DBに登録をする。戻り値は自動発番されたIDが返ってきます
+    $id = $company->insert($data);
+
+    //今登録された情報を発番されたIDで引き、会員情報を取得します（会員登録後の自動ログイン処理のため）
+    $result = $company->select(array("id" => $id), "", "", 1, false);
+
+    $data_s = [];
+
+    //$param = [];
+    //セッションにユーザー情報を登録（ログイン処理）
+    $this->session->set('user_info', $result);
+    //$param["datetime"] = $data["datetime"];
+    $data_s["act"] = $company->select([],"datetime","DESC",1,false);
+
+    $data_s["result"] = $company->select([], "datetime","DESC",5,true);
+
+    array_shift($data_s["result"]);
+    // 登録完了ページを表示します。
+    return $this->view->render($response, 'company-import/company-import_done.twig', $data_s);
+
+});
