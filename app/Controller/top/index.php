291a0af130aac590a0f21e885ffce4db6884a8b6
t-yamamoto-mercury-inc <t-yamamoto@mercury-inc.cc>
2019-08-23 15:39:25 +0900

    fix image

--- app/Controller/top/index.php
+++ app/Controller/top/index.php
@@ -1,12 +1,19 @@
 <?php
 
+use Model\Dao\Company;
 use Slim\Http\Request;
 use Slim\Http\Response;
+
 
 // TOPページのコントローラ
 $app->get('/', function (Request $request, Response $response) {
 
-    $data = [];
+    $data=[];
+    //レビューDAOをインスタンス化します。
+    $reviewList = new Company($this->db);
+    //レビュー一覧を取得し、戻り値をresultに格納します
+    $data["result"] = $reviewList->getReviewList("株式会社カツラマッチ");
+
 
     // Render index view
     return $this->view->render($response, 'top/index.twig', $data);
