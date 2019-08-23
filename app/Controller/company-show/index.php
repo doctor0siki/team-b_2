7da160676f12b03c4713164f71439cefc57db946
shinketya <shinketya@gmail.com>
2019-08-23 14:04:32 +0900

    [fix] company-show/index.phpの不要なところを削除

--- app/Controller/company-show/index.php
+++ app/Controller/company-show/index.php
@@ -2,15 +2,6 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 use Model\Dao\Company;
-//$app->get('/company-show/', function (Request $request, Response $response) {
-
-    //GETされた内容を取得します。
-    //$data = $request->getQueryParams();
-
-    // Render index view
-    //return $this->view->render($response, 'company-show/reviewList.twig', $data);
-
-//});
 
 $app->get('/company-show/', function (Request $request, Response $response) {
 
